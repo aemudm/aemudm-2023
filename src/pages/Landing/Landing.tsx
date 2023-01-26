770bba5c2784a4d9e65aff41cb81a45147525b20
Sid Ali Terkmane <sidterkmane@hotmail.com>
2023-01-26 16:41:26 -0500

    folder structure

---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Landing.scss'
+
+import Navbar from '../components/Navbar/Navbar';
+
+const Landing = () => {
+  return (
+    <body>
+        <Navbar/>
+        <div>Landing</div>
+    </body>
+  )
+}
+
+export default Landing
