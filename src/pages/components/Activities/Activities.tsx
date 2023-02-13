267aa7ce232340109e9fd74b6e141c30c8302364
Sid Ali Terkmane <sidterkmane@hotmail.com>
2023-02-12 23:45:12 -0500

    added assets

--- src/pages/components/Activities/Activities.tsx
+++ src/pages/components/Activities/Activities.tsx
@@ -3,13 +3,18 @@ import './Activities.scss'
 
 import Card from './components/Card/Card'
 
+import dua from '../../../assets/dua.png'
+import iftar from '../../../assets/iftar.png'
+import basket from '../../../assets/basket.png'
+
 const Activities = () => {
   return (
     <div className='activities-section'>
+        <h1>Nos activités</h1>
         <div className="activities">
-            <Card title='Activités annuelles' image='' description=''/>
-            <Card title='Activités réccurentes' image='' description=''/>
-            <Card title='Activités ponctuelles' image='' description=''/>
+            <Card title='Activités annuelles' image={iftar} description=''/>
+            <Card title='Activités réccurentes' image={dua} description=''/>
+            <Card title='Activités ponctuelles' image={basket} description=''/>
         </div>
     </div>
   )
